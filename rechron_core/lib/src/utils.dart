11bb2472c157c7c0794938197f256fc656eed9d5
Mensch <mensch272@users.noreply.github.com>
2022-09-20 22:51:21 +0500

    docs: add documentation to utils.

--- rechron_core/lib/src/utils.dart
+++ rechron_core/lib/src/utils.dart
@@ -1,7 +1,10 @@
+/// Return whether the [year] is a leap year
 bool isLeapYear(int year) =>
     (year % 4 == 0) && ((year % 100 != 0) || (year % 400 == 0));
 
-extension Sub on RegExp {
+extension RegSub on RegExp {
+  /// Return the string obtained by replacing the leftmost non-overlapping
+  /// occurences of pattern in string by the replacement.
   String sub(String replacement, String source) {
     return source.replaceAllMapped(this, (match) {
       if (match.groupCount > 0) {
